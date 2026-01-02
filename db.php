6ffa5e987cee09e885a48be5cc17651932eb5eb1
Manisha Kumari <154701374+manishaku2004@users.noreply.github.com>
2026-01-02 09:14:35 +0530

    db change

--- db.php
+++ db.php
@@ -1,9 +1,10 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "todolist");
+$conn = new mysqli("sql210.infinityfree.com", "if0_40771087", "PMd4UB2BUK5", "if0_40771087_todolist");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 // echo "Connected Successfully";
 ?>
+
 
