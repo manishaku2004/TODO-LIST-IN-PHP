928f8771dc2505614e03cccd7cbe91abf5454875
Manisha Kumari <154701374+manishaku2004@users.noreply.github.com>
2026-01-02 09:57:47 +0530

    change crediential

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("sql210.infinityfree.com", "if0_40771087", "PMd4UB2BUK5", "if0_40771087_todolist");
+$conn = new mysqli("localhost","root","","todolist");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
